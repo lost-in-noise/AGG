a135d18be7fdf8432041333f2c4aea1ada56f1f9
Lost-in-Noise <nancykikvadze@gmail.com>
2023-09-18 11:39:18 +0400

    added burger bar

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
+'use strict';
+
 document.addEventListener('DOMContentLoaded', function() {
     const registerButton = document.getElementById('register');
 
     registerButton.addEventListener('click', function() {
-        
+        // registration form here
         alert('You clicked the Register button!');
     });
 });
+
+
+
+
+
+
+
+//burger bar
+let navBar = document.getElementById('navBar');
+let burgerBar = document.getElementById('burgerBar');
+let navDiv = document.getElementById('navDiv');
+
+burgerBar.addEventListener('click', function() {
+    navBar.classList.toggle('activeNav');
+    burgerBar.classList.toggle('active');
+    navDiv.classList.toggle('mobActive');
+})
